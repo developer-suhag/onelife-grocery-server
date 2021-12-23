2308481454b8d994d018d51c549b06d8330681d4
Suhag Al Amin <developersuhag@gmail.com>
2021-12-23 09:56:18 +0600

    test

--- index.js
+++ index.js
@@ -48,24 +48,24 @@ async function run() {
       res.send(result);
     });
 
-    // post order api
-    app.post("/orders", async (req, res) => {
-      const payment = req.body;
-      const result = orderCollection.insertOne(payment);
-      res.json(result);
-    });
+    // // post order api
+    // app.post("/orders", async (req, res) => {
+    //   const payment = req.body;
+    //   const result = orderCollection.insertOne(payment);
+    //   res.json(result);
+    // });
 
-    // stripe payment
-    app.post("/create-payment-intent", async (req, res) => {
-      const paymentInfo = req.body;
-      const amount = paymentInfo.price * 100;
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: amount,
-        currency: "usd",
-        payment_method_types: ["card"],
-      });
-      res.json({ clientSecret: paymentIntent.client_secret });
-    });
+    // // stripe payment
+    // app.post("/create-payment-intent", async (req, res) => {
+    //   const paymentInfo = req.body;
+    //   const amount = paymentInfo.price * 100;
+    //   const paymentIntent = await stripe.paymentIntents.create({
+    //     amount: amount,
+    //     currency: "usd",
+    //     payment_method_types: ["card"],
+    //   });
+    //   res.json({ clientSecret: paymentIntent.client_secret });
+    // });
 
     //
     //
